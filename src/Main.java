7c8e197644eb44a3783bbc89adcd48e2dcfc2656
joaocandidozup <joao.candido@zup.com>
2024-12-13 13:50:11 -0300

    feat: add function display menu

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    public static void exibirMenu(){
+        String menu = """
+                Bem vindo ao Jogo do Adivinha!!!
+                1 - Iniciar o jogo
+                2 - Sair do jogo
+                Digite uma opção
+                """;
+        System.out.println(menu);
+    }
 }
