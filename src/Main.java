47f8962fc7151098e658e1e8a82ebb5b5e961e1e
joaocandidozup <joao.candido@zup.com>
2024-12-13 15:04:20 -0300

    feat: add the function calculates points

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
         int opcao = -1;
+        int pontos = 0;
         while (opcao !=2){
             exibirMenu();
             opcao = leitura.nextInt();
@@ -14,7 +15,8 @@ public class Main {
                 int numeroAleatorio = gerarNumeroAleatorio(intervalo(nivel));
                 System.out.println("Digite um numero inteiro");
                 int palpite = lerPalpite(leitura);
-
+                pontos += calcularPontos(palpite,numeroAleatorio);
+                System.out.println("Sua pontuação atual: "+ pontos);
             }
         }
     }
@@ -50,5 +52,14 @@ public class Main {
             leitura.next();
         }
         return leitura.nextInt();
+    }
+    public static int calcularPontos(int palpite, int numeroAleatorio){
+        if(palpite == numeroAleatorio){
+            return 10;
+        } else if (palpite == numeroAleatorio +1 || palpite == numeroAleatorio -1){
+            return 5;
+        }else {
+            return 0;
+        }
     }
 }
